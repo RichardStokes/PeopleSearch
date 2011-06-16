64075e907b203fe3855f47b907aa01e76b0fd9f8
Richard Stokes <richardstokes@live.com>
2011-06-16 22:18:10 +0100

    Removed redundant code

--- googleajax_test.rb
+++
@@ -1,8 +0,0 @@
-require 'rubygems'
-require 'googleajax'
-
-GoogleAjax.referer = "ucd.ie"
-results = GoogleAjax::Search.web("Hello World!")
-puts results.class
-results = results.to_hash
-results.each { |result| puts result.to_s}
